3004a82ad59c9e7a82c1cfe562eba940b742bcb4
HAA02 <88357250+HAA02@users.noreply.github.com>
2022-05-05 23:35:25 +0900

    5/5 Item Spawner

--- TPS Start Project/Assets/Scripts/ItemSpawner.cs
+++ TPS Start Project/Assets/Scripts/ItemSpawner.cs
@@ -16,16 +16,27 @@ public class ItemSpawner : MonoBehaviour
 
     private void Start()
     {
-
+        timeBetSpawn = Random.Range(timeBetSpawnMin, timeBetSpawnMax);
+        lastSpawnTime = 0f;
     }
 
     private void Update()
     {
+        if(Time.time >= lastSpawnTime + timeBetSpawn && playerTransform != null)
+        {
+            Spawn();
+            lastSpawnTime = Time.time;
+            timeBetSpawn = Random.Range(timeBetSpawnMin, timeBetSpawnMax);
 
+        }
     }
 
     private void Spawn()
     {
+        var spwanPosition = Utility.GetRandomPointOnNavMesh(playerTransform.position, maxDistance, NavMesh.AllAreas);
+        var item = Instantiate(items[Random.Range(0, items.Length)], spwanPosition, Quaternion.identity);
+        Destroy(item, 5f);
 
+        spwanPosition += Vector3.up + 0.5f;
     }
 }
